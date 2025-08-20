84e8d6b75e29e08bf3bef1f575817cb139659d1f
Saktisruti Jena <saktisruti53@gmail.com>
2025-08-20 15:52:35 +0530

    inc/dec

---
+++ src/Increment.java
@@ -0,0 +1,11 @@
+class Increment
+{
+public static void main (String []args)
+{
+  int a = 5;
+  int b =6;
+System.out.println(++a); //Pre increment
+System.out.println(b++); //Post increment
+
+}
+}
